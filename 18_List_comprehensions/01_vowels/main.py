05a7f3c1815f389a992e66a25a92b7eb50fe9ac2
Stas Yu <stasugai101@gmail.com>
2023-06-21 22:33:08 +0600

    task_1

--- 18_List_comprehensions/01_vowels/main.py
+++ 18_List_comprehensions/01_vowels/main.py
@@ -1,1 +1,6 @@
-# TODO здесь писать код
+adjective = ['а', 'и', 'е', 'ё', 'о', 'у', "ы", 'э', 'ю', 'я']
+text = list(input('Введите текст: '))
+
+adj_list = [i_let for i_let in adjective for i_sym in text if i_let == i_sym]
+print('Список гласных букв: ', adj_list)
+print('Длина списка: ', len(adj_list))
