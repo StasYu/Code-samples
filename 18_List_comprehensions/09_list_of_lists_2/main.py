c0f51a0bdfa7e368daf36cac357b118c74057fff
Stas Yu <stasugai101@gmail.com>
2023-06-21 22:47:27 +0600

    task_9

--- 18_List_comprehensions/09_list_of_lists_2/main.py
+++ 18_List_comprehensions/09_list_of_lists_2/main.py
@@ -1,4 +1,11 @@
 nice_list = [[[1, 2, 3], [4, 5, 6], [7, 8, 9]],
              [[10, 11, 12], [13, 14, 15], [16, 17, 18]]]
 
-# TODO здесь писать код
+
+new_list = [
+    i_num for i_list in range(len(nice_list))
+    for i_sym in range(len(nice_list[i_list]))
+    for i_num in nice_list[i_list][i_sym]
+]
+
+print(new_list)
