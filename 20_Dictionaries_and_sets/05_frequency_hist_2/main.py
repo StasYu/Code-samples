4a49ea71bf08f0990ea6a4e4b0b868ee7d713eb7
Stas Yu <stasugai101@gmail.com>
2023-07-01 22:06:38 +0600

    task_20.5

--- 20_Dictionaries_and_sets/05_frequency_hist_2/main.py
+++ 20_Dictionaries_and_sets/05_frequency_hist_2/main.py
@@ -1,1 +1,20 @@
-# TODO здесь писать код
+text = input('Введите текст: ')
+
+unique_text = set(text)
+inverted_dict = dict()
+text_dict = {i_sym: text.count(i_sym) for i_sym in unique_text}
+
+print('Оригинальный словарь частот:')
+
+
+for i_sym in text_dict:
+    print(f'{i_sym} : {text_dict[i_sym]}')
+
+
+print('\nИнвертированный словарь частот:')
+
+for i_key in range(1, max(text_dict.values()) + 1):
+    temp_list = [j_sym for j_sym in text_dict if text_dict[j_sym] == i_key]
+    print(f'{i_key} : {temp_list}')
+    inverted_dict.update({i_key:temp_list})
+
