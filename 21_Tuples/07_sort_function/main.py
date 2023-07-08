4ae8812d982f360b12fe3c268ff94799763dbf21
Stas Yu <stasugai101@gmail.com>
2023-07-09 02:14:01 +0600

    task 7

--- 21_Tuples/07_sort_function/main.py
+++ 21_Tuples/07_sort_function/main.py
@@ -1,1 +1,21 @@
-# TODO здесь писать код
+# Функция сортировки Напишите функцию, которая сортирует кортеж, состоящий
+# из целых чисел по возрастанию и возвращает его.
+# Если хотя бы один элемент не является целым числом,
+# то функция возвращает исходный кортеж.
+
+num_tup = (2, 8, 9, 3, 1, 4, 5, 6, 7)
+flag = True
+
+for i in num_tup:
+    if i != int(i):
+        flag = False
+if flag:
+    num_list = list(num_tup)
+    num_tup = tuple(sorted(num_list))
+
+print(num_tup)
+
+
+
+
+# new_nums = [i for i in num_tup]
