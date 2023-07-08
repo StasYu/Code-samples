0aa5b77a12ff9b172d1419cc2b30d63637332d47
Stas Yu <stasugai101@gmail.com>
2023-07-09 03:56:08 +0600

    task 8

--- 21_Tuples/08_contacts_3/main.py
+++ 21_Tuples/08_contacts_3/main.py
@@ -1,1 +1,22 @@
-# TODO здесь писать код
+phonebook = {('Ксения', 'Мельникова'): 76876868, ('София', 'Иванова'): 1233211, ('Мария', 'Власова'): 132712973}
+
+while True:
+    quest = input('enter the action necessary: ').lower()
+
+    if quest == 'добавить контакт':
+        name = input('enter name and surname: ').split()
+        name = tuple(name)
+
+        if name in phonebook.keys():
+            print('\nName is already exist in phonebook')
+
+        number = int(input('enter phone number: '))
+        phonebook[name] = number
+        print(phonebook)
+
+    elif quest == 'поиск человека по фамилии':
+        surname = input('enter surname: ')
+
+        for i in phonebook:
+            if i[1] == surname:
+                print('\n', i[0], i[1], phonebook[i])
