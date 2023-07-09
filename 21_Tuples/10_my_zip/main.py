7e1bcf0fa56d6751ece0845e6a358c74aaeeaadc
Stas Yu <stasugai101@gmail.com>
2023-07-09 14:57:10 +0600

    task 10

--- 21_Tuples/10_my_zip/main.py
+++ 21_Tuples/10_my_zip/main.py
@@ -1,1 +1,12 @@
-# TODO здесь писать код
+string = input('Строка: ')
+num_tuples = input('Кортеж чисел: ')
+
+# string = 'abcd'
+# num_tuples = (10, 20, 30, 40)
+
+zipped_string = zip(string, num_tuples)
+
+print('Результат:')
+print(zipped_string)
+for i in zipped_string:
+    print(i)
