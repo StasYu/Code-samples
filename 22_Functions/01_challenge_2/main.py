2cda333cdfaf124f017690738c7ce63f7ec45805
Stas Yu <stasugai101@gmail.com>
2023-07-11 00:11:58 +0600

    task 1

--- 22_Functions/01_challenge_2/main.py
+++ 22_Functions/01_challenge_2/main.py
@@ -1,1 +1,9 @@
-# TODO здесь писать код
+def list_of_nums(num, new_num = 0):
+    new_num += 1
+    print(new_num)
+    if new_num != num:
+        list_of_nums(num,new_num)
+
+
+num_q = int(input('enter number q-ty: '))
+result = list_of_nums(num_q)
