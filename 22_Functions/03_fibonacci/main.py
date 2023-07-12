a744fc02dc107fb7be47c8698292229f7dd5987f
Stas Yu <stasugai101@gmail.com>
2023-07-12 17:51:16 +0600

    task 3

--- 22_Functions/03_fibonacci/main.py
+++ 22_Functions/03_fibonacci/main.py
@@ -1,1 +1,11 @@
-# TODO здесь писать код
+num_pos = int(input('Введите позицию числа в ряде Фибоначчи: '))
+init_num = [1, 1]
+
+def new_init(count, fibo=init_num):
+    new_num = sum(fibo[count-2:])
+    fibo.append(new_num)
+
+
+fibo_num = [new_init(i) for i in range(num_pos) if i >= 2]
+
+print(f'\nЧисло: {init_num[len(init_num) - 1]}')
