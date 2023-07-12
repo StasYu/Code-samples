a26c12ef23e32164eea77edf6b5d156fffb6b4d6
Stas Yu <stasugai101@gmail.com>
2023-07-12 18:12:05 +0600

    task 4

--- 22_Functions/04_search_element_2/main.py
+++ 22_Functions/04_search_element_2/main.py
@@ -1,1 +1,25 @@
-# TODO здесь писать код
+site = {
+	'html': {
+		'head': {
+			'title': 'Мой сайт'
+		},
+		'body': {
+			'h2': 'Здесь будет мой заголовок',
+			'div': 'Тут, наверное, какой-то блок',
+			'p': 'А вот здесь новый абзац'
+		}
+	}
+}
+
+user_key = input('enter necessary key: ')
+
+def find_key(key,site):
+    if key in site.keys():
+        return site[key]
+    if isinstance(site.keys(), dict):
+        find_key(key, site.keys())
+    else:
+        print('key is not found')
+
+
+print(find_key(user_key, site))
