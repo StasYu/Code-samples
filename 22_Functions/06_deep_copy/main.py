9135d4440bfd36f10e76b4d6769161aedbeae5a9
Stas Yu <stasugai101@gmail.com>
2023-07-14 13:25:22 +0600

    task 6

--- 22_Functions/06_deep_copy/main.py
+++ 22_Functions/06_deep_copy/main.py
@@ -1,14 +1,37 @@
-site = {
-    'html': {
-        'head': {
-            'title': 'Куплю/продам телефон недорого'
-        },
-        'body': {
-            'h2': 'У нас самая низкая цена на iphone',
-            'div': 'Купить',
-            'p': 'продать'
-        }
-    }
-}
+# site = {
+#     'html': {
+#         'head': {
+#             'title': 'Куплю/продам телефон недорого'
+#         },
+#         'body': {
+#             'h2': 'У нас самая низкая цена на iphone',
+#             'div': 'Купить',
+#             'p': 'продать'
+#         }
+#     }
+# }
 
-# TODO здесь писать код
+def struct(count):
+    new_count = count - 1
+    name = input('\nВведите название продукта для нового сайта: ')
+    print(f"\nСайт для {name}:")
+    print("site = {")
+    print("    'html': {")
+    print("        'head': {")
+    print("           'title': 'Куплю/продам телефон недорого")
+    print("        },")
+    print("        'body': {")
+    print(f"            'h2': f'У нас самая низкая цена на {name}',")
+    print("            'div': 'Купить',")
+    print("           'p': 'продать'")
+    print("       }")
+    print("   }")
+    print("}")
+    if new_count == 0:
+        return None
+    struct(new_count)
+
+
+site_qtt = int(input('Сколько сайтов: '))
+struct(site_qtt)
+
