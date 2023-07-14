382e1ab7f81d439886cc07a55a8d78feff056761
Stas Yu <stasugai101@gmail.com>
2023-07-14 12:13:53 +0600

    task 5

--- 22_Functions/05_make_function_faster/main.py
+++ 22_Functions/05_make_function_faster/main.py
@@ -1,9 +1,18 @@
-def calculating_math_func(data):
-    result = 1
-    for index in range(1, data + 1):
-        result *= index
-    result /= data ** 3
-    result = result ** 10
-    return result
 
-# TODO оптимизировать функцию
+def calculating_math_func(data, result={1:1}):
+    if data in result:
+        return result[data]
+    temp = 1
+    for index in range(1, data + 1):
+        temp *= index
+    temp /= data ** 3
+    temp = temp ** 10
+    result[data] = temp
+    print(result)
+    return result[data]
+# Оптимизировал функцию чтобы не высчитывать результат
+# того же числа, вместо факториала
+
+while True:
+    number = int(input('enter new num: '))
+    print(f'first answer: {calculating_math_func(number)}')
