9eeaf9943ca2237e069c4562839337850b7f50d6
Stas Yu <stasugai101@gmail.com>
2023-07-22 18:02:14 +0600

    task 2

--- 23_Files/02_zen_of_python/main.py
+++ 23_Files/02_zen_of_python/main.py
@@ -1,1 +1,10 @@
-# TODO здесь писать код
+zen_file = open('zen.txt', 'r')
+new_file = open('new_zen', 'a')
+
+for i in zen_file:
+    new_file.write(i)
+
+new_file.close()
+new_file = open('new_zen', 'r')
+
+print(new_file.read())
