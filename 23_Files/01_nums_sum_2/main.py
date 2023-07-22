0e9d1fd029c2260d40ad93f419133226e30102c7
Stas Yu <stasugai101@gmail.com>
2023-07-22 17:51:21 +0600

    task 1

--- 23_Files/01_nums_sum_2/main.py
+++ 23_Files/01_nums_sum_2/main.py
@@ -1,1 +1,12 @@
-# TODO здесь писать код
+file = open('numbers.txt', 'r')
+temp_num = 0
+for i_line in file:
+    temp = i_line.split()
+    if len(temp) > 0:
+        temp_num += int(temp[0])
+
+file.seek(0)
+print('Содержимое файла numbers.txt\n', file.read())
+print()
+file.close()
+print('Содержимое файла answer.txt\n', temp_num)
