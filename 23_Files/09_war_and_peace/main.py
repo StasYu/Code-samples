13f9aa5eedf391650a0d8d26e54ae6f2d8f2b381
Stas Yu <stasugai101@gmail.com>
2023-07-27 01:24:56 +0600

    task_9

--- 23_Files/09_war_and_peace/main.py
+++ 23_Files/09_war_and_peace/main.py
@@ -1,1 +1,30 @@
-# TODO здесь писать код
+import zipfile
+import string
+
+zfile = zipfile.ZipFile('voyna-i-mir.zip', 'r')
+name = zfile.namelist()
+zfile.extractall()
+zfile.close()
+
+alpha = string.ascii_letters
+
+
+
+def counter(name):
+    file = open(name[0], 'r')
+    result = dict()
+    for i_line in file:
+        for j_sym in i_line:
+            if j_sym.isalpha():
+                if j_sym not in result:
+                    result[j_sym] = 0
+                result[j_sym] += 1
+    file.close()
+    return result
+
+let_dict = counter(name)
+
+let_dict = dict(sorted(let_dict.items(), key= lambda x: x[1], reverse=True))
+
+for i,j in let_dict.items():
+    print(i,j)
