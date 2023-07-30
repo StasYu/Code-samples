0c1c43b740e03bac82a079c3d5170c5acecd92c4
Stas Yu <stasugai101@gmail.com>
2023-07-30 12:53:09 +0600

    task_5

--- 24_Exceptions/05_text_calc/main.py
+++ 24_Exceptions/05_text_calc/main.py
@@ -1,1 +1,33 @@
-# TODO здесь писать код
+def operations(operands):
+    if operands[1] == '+':
+        operation = int(operands[0]) + int(operands[2])
+    elif operands[1] == '-':
+        operation = int(operands[0]) - int(operands[2])
+    elif operands[1] == '*':
+        operation = int(operands[0]) * int(operands[2])
+    elif operands[1] == '/':
+        operation = int(operands[0]) / int(operands[2])
+    elif operands[1] == '//':
+        operation = int(operands[0]) // int(operands[2])
+    elif operands[1] == '%':
+        operation = int(operands[0]) % int(operands[2])
+    return operation
+
+temp = 0
+
+with open('calc.txt', 'r') as calc_file:
+    for i_line in calc_file:
+        operands = i_line.split()
+        try:
+            result = operations(operands)
+            temp += result
+        except ValueError:
+            print('Введите число')
+        except SyntaxError:
+            print('Syntax Error')
+        except UnboundLocalError:
+            print('Не введено действие')
+        except IndexError:
+            print('Не хватает строк')
+
+print(temp)
