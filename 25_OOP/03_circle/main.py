6d57347bcb374a867cb50f49c6550d2ca3bc248f
Stas Yu <stasugai101@gmail.com>
2023-08-03 01:28:58 +0600

    task 3

--- 25_OOP/03_circle/main.py
+++ 25_OOP/03_circle/main.py
@@ -1,1 +1,9 @@
-# TODO здесь писать код
+from circle import Circle
+
+circle_1 = Circle(0, 0, 1)
+circle_2 = Circle(1.5, 1.5, 1.2)
+
+circle_1.square()
+circle_1.perimetr()
+circle_1.grow(2)
+circle_1.is_cross(circle_2)
