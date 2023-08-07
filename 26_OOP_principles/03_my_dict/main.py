568057db5c669c0d60845ca2575c6d79588afdda
Stas Yu <stasugai101@gmail.com>
2023-08-08 02:41:39 +0600

    task 3

--- 26_OOP_principles/03_my_dict/main.py
+++ 26_OOP_principles/03_my_dict/main.py
@@ -1,1 +1,14 @@
-# TODO здесь писать код
+class MyDict(dict):
+
+    def get(self, __key):
+        for i_key, i_value in self.items():
+            if i_key == __key:
+                return i_value
+            else:
+                return 0
+
+new_dict = MyDict()
+new_dict[1] = 1
+new_dict[2] = 2
+print(new_dict.get(4))
+
